fa843620942daa8e50e487c8886696597c944d22
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-10 18:36:35 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,19 +2,10 @@
 from main import data_summary
 import pandas as pd
 
-# Load the dataset
-my_df = pd.read_csv('cars.csv', sep = ';')
-
-
-def test_summary(data):
-    summary = data_summary(data)
-
-    mean = data['Weight'].mean()
-    median = data['Weight'].median()
-
+def test_summary():
+    my_df = pd.read_csv('cars.csv', sep=';')
+    summary = data_summary(my_df)
+    mean = my_df['Weight'].mean()
+    median = my_df['Weight'].median()
     assert mean == summary.loc['mean', 'Weight'], "Mean test failed"
     assert median == summary.loc['50%', 'Weight'], "Median test failed"
-    print("All Test passed!")
-
-if __name__ == "__main__":
-    test_summary(my_df)
