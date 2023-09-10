58f258276e450c3e43c0be9cc2f014d0fd350b4e
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-10 18:44:45 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -9,3 +9,17 @@ def test_summary():
     median = my_df['Weight'].median()
     assert mean == summary.loc['mean', 'Weight'], "Mean test failed"
     assert median == summary.loc['50%', 'Weight'], "Median test failed"
+
+def test_load_data():
+    data_path = 'cars.csv'
+    df = load_data(data_path)
+    assert isinstance(df, pd.DataFrame), "load_data should return a DataFrame"
+    assert not df.empty, "DataFrame should not be empty"
+
+def test_data_visual():
+    data_path = 'cars.csv'
+    df = load_data(data_path)
+    try:
+        data_visual(df)
+    except Exception as e:
+        assert False, f"data_visual function raised an exception: {e}"
