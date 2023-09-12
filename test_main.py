6feafb5cb71f0fea7227769a5397eca9bdb0e144
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-12 15:34:56 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,19 +1,19 @@
 from main import data_summary, load_data, data_visual
-import pandas as pd
+import polars as pl
 
 def test_summary():
-    my_df = pd.read_csv('cars.csv', sep=';')
+    my_df = pl.read_csv('cars.csv', separator=';')
     summary = data_summary(my_df)
-    mean = my_df['Weight'].mean()
-    median = my_df['Weight'].median()
-    assert mean == summary.loc['mean', 'Weight'], "Mean test failed"
-    assert median == summary.loc['50%', 'Weight'], "Median test failed"
+    mean = my_df['Weight'].mean().get(0)
+    median = my_df['Weight'].median().get(0)
+    assert mean == summary['mean']['Weight'], "Mean test failed"
+    assert median == summary['50%']['Weight'], "Median test failed"
 
 def test_load_data():
     data_path = 'cars.csv'
     df = load_data(data_path)
-    assert isinstance(df, pd.DataFrame), "load_data should return a DataFrame"
-    assert not df.empty, "DataFrame should not be empty"
+    assert isinstance(df, pl.DataFrame), "load_data should return a DataFrame"
+    assert not df.shape[0] == 0, "DataFrame should not be empty"
 
 def test_data_visual():
     data_path = 'cars.csv'
