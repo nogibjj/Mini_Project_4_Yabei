f0fddf4ada76e97a5b19daaaca630e5831cba12f
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-09-22 19:02:18 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,23 +1,5 @@
-from main import data_summary, load_data, data_visual
-import polars as pl
+import main
+import sys
 
-def test_summary():
-    my_df = pl.read_csv('cars.csv', separator=';')
-    summary = data_summary(my_df)
-    mean = my_df['Weight'].mean()
-    median = my_df['Weight'].median()
-
-
-def test_load_data():
-    data_path = 'cars.csv'
-    df = load_data(data_path)
-    assert isinstance(df, pl.DataFrame), "load_data should return a DataFrame"
-    assert not df.shape[0] == 0, "DataFrame should not be empty"
-
-def test_data_visual():
-    data_path = 'cars.csv'
-    df = load_data(data_path)
-    try:
-        data_visual(df)
-    except Exception as e:
-        assert False, f"data_visual function raised an exception: {e}"
+def test_get_python_version():
+    assert main.get_python_version() == sys.version
